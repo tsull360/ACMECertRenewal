b42ba3f0bf8473c6143cb9d8d941cfc0cf7b4db7
Tim S <tsull360@outlook.com>
2026-03-05 20:10:42 -0700

    Initial commit

---
+++ Renew-LetsEncryptCert.ps1
@@ -0,0 +1,13 @@
+﻿# you need to supply a email later the first time you create a certificate for a domain, i just always do it with a variable
+$CFemail = ""
+
+# supply the hostname that matches the record you created at cloudflare
+$hostname = ""
+
+# supply your API token created above as a secure string within the $pArgs hash table. This will have the effect of prompting you to type or paste your token into the screen.
+$pArgs = @{
+    CFToken = (Read-Host 'API Token' -AsSecureString)
+}
+
+#this string will create the certificate and install it automatically into the Windows certificates store
+New-PACertificate $hostname -Plugin Cloudflare -PluginArgs $pArgs -Install -AcceptTOS -Contact $CFemail -Force
---
+++ Rotate-IISSSLCert.ps1
@@ -0,0 +1,8 @@
+﻿$OldThumbprint = ""
+$NewThumbprint = ""
+
+Get-WebBinding | Where-Object { $_.certificateHash -eq $OldThumbprint} | ForEach-Object {
+    Write-Host "Replacing Cert For "  $_ 
+    $_.RemoveSslCertificate()
+    $_.AddSslCertificate($NewThumbprint, 'My')
+}
